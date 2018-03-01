d0c83cabca962e40c87584d3716dacfdf1eacb1e
CTzz <ctzz@protonmail.com>
2018-03-01 23:41:08 +0800

    First commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+
+import App from './components/app';
+import Thunk from 'redux-thunk';
+import reducers from './reducers';
+
+const store = createStore(reducers, applyMiddleware(Thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.querySelector('#app'));
