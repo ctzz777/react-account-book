6eccdc68de244505ee078db3a35ac7fc8b8c9637
CTzz <ctzz@protonmail.com>
2018-04-01 19:58:03 +0800

    Add category action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 import axios from 'axios';
 
-const authToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhYjI5MjgwZTU3MWEyMTY5MzNmZDIxNCIsImlhdCI6MTUyMjI1NDA5MywiZXhwIjoxNTIyMzQ0MDkzfQ.ljpnzfbDNOEA8pWRVczhg28Y3lDLybSv46k_uyka3n4';
+const authToken = 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhYjI5MjgwZTU3MWEyMTY5MzNmZDIxNCIsImlhdCI6MTUyMjU4Mjk4MywiZXhwIjoxNTIyNjcyOTgzfQ.mS8H-HLcZcDXVSX3w3S6w3nQrw6qFnqM_G3loVmdkaA';
 axios.defaults.baseURL = 'http://localhost:3001';
 axios.defaults.headers.common['Authorization'] = authToken;
 
@@ -13,41 +13,76 @@ export const FETCH_ACCOUNTS_REQUEST = 'FETCH_ACCOUNTS_REQUEST';
 export const FETCH_ACCOUNTS_SUCCESS = 'FETCH_ACCOUNTS_SUCCESS';
 export const FETCH_ACCOUNTS_FAILURE = 'FETCH_ACCOUNTS_FAILURE';
 
-export const setDate = date => {
+export function setDate(date) {
   return {
     type: SET_DATE,
     date,
   };
-}
+};
 
-const fetchAccountsRequest = () => {
+function fetchAccountsRequest() {
   return {
     type: FETCH_ACCOUNTS_REQUEST
   };
 };
 
-const fetchAccountsSuccess = (accounts) => {
+function fetchAccountsSuccess(accounts) {
   return {
     type: FETCH_ACCOUNTS_SUCCESS,
     accounts,
   };
 };
 
-const fetchAccountsfailure = (error) => {
+function fetchAccountsfailure(error) {
   return {
     type: FETCH_ACCOUNTS_FAILURE,
     error,
   };
 };
 
-export const fetchAccounts = date => dispatch => {
-  const formatDate = date.format(formatPattern);
-  dispatch(fetchAccountsRequest());
-  axios.get(`/api/account/accountBook/${accountBookId}/date/${formatDate}`)
-    .then((res) => {
-      dispatch(fetchAccountsSuccess(res.data));
-    })
-    .catch((error) => {
-      dispatch(fetchAccountsfailure(error));
-    });
-}
+export function fetchAccounts(date) {
+  return (dispatch) => {
+    const formatDate = date.format(formatPattern);
+    dispatch(fetchAccountsRequest());
+    axios.get(`/api/account/accountBook/${accountBookId}/date/${formatDate}`)
+      .then((res) => {
+        dispatch(fetchAccountsSuccess(res.data));
+      })
+      .catch((error) => {
+        dispatch(fetchAccountsfailure(error));
+      });
+  }
+};
+
+function fetchCategoriesRequest() {
+  return {
+    type: FETCH_CATEGORIES_REQUEST
+  };
+};
+
+function fetchCategoriesSuccess(accounts) {
+  return {
+    type: FETCH_CATEGORIES_SUCCESS,
+    categories,
+  };
+};
+
+function fetchCategoriesfailure(error) {
+  return {
+    type: FETCH_CATEGORIES_FAILURE,
+    error,
+  };
+};
+
+export function fetchCategories() {
+  return (dispatch) => {
+    dispatch(fetchCategoriesRequest());
+    axios.get('/api/category')
+      .then((res) => {
+        dispatch(fetchCategoriesSuccess(res.data));
+      })
+      .catch((error) => {
+        dispatch(fetchCategoriesfailure(error));
+      });
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { SET_DATE, FETCH_ACCOUNTS_REQUEST, FETCH_ACCOUNTS_SUCCESS, FETCH_ACCOUNTS_FAILURE } from '../actions';
+import { SET_DATE, FETCH_ACCOUNTS_REQUEST, FETCH_ACCOUNTS_SUCCESS, FETCH_ACCOUNTS_FAILURE, SET_SELECTED_CATEGORY, FETCH_CATEGORIES_REQUEST, FETCH_CATEGORIES_SUCCESS, FETCH_CATEGORIES_FAILURE } from '../actions';
 import moment from 'moment';
 
 const date = (state=moment(), action) => {
@@ -13,7 +13,8 @@ const date = (state=moment(), action) => {
 
 const accounts = (state=[], action) => {
   switch (action.type) {
-    case FETCH_ACCOUNTS_SUCCESS:
+    case FETCH_ACCOUNTS_REQUEST:
+      return state;
     case FETCH_ACCOUNTS_SUCCESS:
       return action.accounts;
     case FETCH_ACCOUNTS_FAILURE:
@@ -23,9 +24,23 @@ const accounts = (state=[], action) => {
   }
 };
 
+const categories = (state=[], action) => {
+  switch (action.type) {
+    case FETCH_CATEGORIES_REQUEST:
+      return state;
+    case FETCH_CATEGORIES_SUCCESS:
+      return action.accounts;
+    case FETCH_CATEGORIES_FAILURE:
+      return [];
+    default:
+      return state;
+  }
+}
+
 const rootReducer = combineReducers({
   date,
   accounts,
+  categories,
 });
 
 export default rootReducer;
