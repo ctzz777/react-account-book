17823eb246e3e587c4d6f6576d7efc6912d0cc25
CTzz <ctzz@protonmail.com>
2018-04-05 00:08:04 +0800

    Complete AddAccount redux form
    1. default value
    2. validation
    3. date field readonly
    4. amount field integer nomorization
    5. change Input to Form.Input

--- src/components/AddAccount.js
+++ src/components/AddAccount.js
@@ -1,8 +1,81 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
+import { Input, Label, Form, TextArea, Button, Dropdown } from 'semantic-ui-react';
+import moment from 'moment';
 import { fetchCategories } from '../actions';
 import CategoryList from './CategoryList';
+
+const validate = values => {
+  const errors = {};
+  const requiredFields = [
+    'amount',
+    'category',
+  ];
+  requiredFields.forEach(field => {
+    if (!values[field]) {
+      errors[field] = 'Required'
+    }
+  });
+  if (values.amount && !Number.isInteger(values.amount)) {
+    console.log(Number.isInteger(values.amount))
+    errors.amount = 'Invalid Amount';
+  };
+  return errors;
+}
+
+const DescriptionField = ({input, label, ...custom}) => (
+  <TextArea
+    rows={3}
+    autoHeight
+    placeholder={label}
+    {...input}
+    {...custom}
+  />
+);
+
+const AmountField = ({input, label, meta: {touched, error}, ...custom}) => (
+  <Form.Input
+    fluid
+    icon="dollar"
+    iconPosition='left'
+    error={!!(touched&&error)}
+    placeholder={label}
+    {...input}
+    {...custom}
+  />
+)
+
+const DateField = ({input, label, ...custom}) => (
+  <Form.Input
+    fluid
+    // disabled
+    icon="calendar"
+    iconPosition='left'
+    placeholder={label}
+    {...input}
+    {...custom}
+  />
+)
+
+const CategorySelectorField = ({ categories, input: { value, onChange }, meta: {touched, error}, label}) => {
+  const options = categories.map(category => ({
+    text: category.name,
+    value: category._id,
+    icon: category.icon,
+  }));
+  return (
+    <Dropdown
+      selection
+      value={value}
+      onChange={(event, data) => onChange(data.value)}
+      fluid
+      error={!!(touched&&error)}
+      placeholder={label}
+      options={options}
+    />
+  );
+};
 
 class AddAccount extends Component {
   constructor(props) {
@@ -17,26 +90,66 @@ class AddAccount extends Component {
   render() {
     const { categories, handleSubmit } = this.props;
     return (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <Field
-            name="category"
-            label="Select Category"
-            component={CategoryList}
-            categories={categories}
-          />
+      <Form onSubmit={handleSubmit}>
+        <div className="ui grid">
+          <div className="row">
+            <div className="column">
+              <label><b>Date</b></label>
+              <Field
+                name="date"
+                label="Input Date"
+                format={value => moment(value).format('YYYY/MM/DD')}
+                normalize={(value, previousValue) => previousValue}
+                component={DateField}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="column">
+              <label><b>Amount</b></label>
+              <Field
+                name="amount"
+                type="number"
+                label="Input Amount"
+                normalize={value => isNaN(parseInt(value, 10)) ? null : parseInt(value, 10)}
+                component={AmountField}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="column">
+              <label><b>Category</b></label>
+              <Field
+                name="category"
+                label="Select Category"
+                component={CategorySelectorField}
+                categories={categories}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="column">
+              <label><b>Description</b></label>
+              <Field
+                name="description"
+                label="Input Description"
+                component={DescriptionField}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="column">
+              <Button positive type="submit">Submit</Button>
+            </div>
+          </div>
         </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      </Form>
     );
   }
 };
 
 const mapStateToProps = state => {
   return {
-    date: state.date.format('YYYYMMDD'),
     categories: state.categories,
   };
 }
@@ -45,7 +158,13 @@ AddAccount = connect(
   mapStateToProps,
 )(AddAccount);
 
-export default reduxForm({
+export default connect(
+  state => ({
+    initialValues: {date: state.date.format('YYYYMMDD')} // pull initial values from account reducer
+  }),
+)(reduxForm({
   form: 'AddAccount',
-  onSubmit: (value) => alert(JSON.stringify(value, null, 2))
-})(AddAccount);
+  validate,
+  onSubmit: (value) => alert(JSON.stringify(value, null, 2)),
+  enableReinitialize: true,
+})(AddAccount));
