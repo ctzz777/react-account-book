d17ac820b0ee30f8878ae457ee6f55694d4d2651
CTzz <ctzz@protonmail.com>
2018-04-06 00:53:58 +0800

    Add ReactRouter

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import AccountPage from './AccountPage';
+import AddAccount from './AddAccount';
 
 export default class App extends Component {
   render() {
     return (
-      <AccountPage />
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/addAccount">AddAccount</Link>
+          <hr />
+          <Route path="/" exact component={AccountPage} />
+          <Route path="/addAccount" component={AddAccount} />
+        </div>
+      </Router>
     );
   }
 }
